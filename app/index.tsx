e1f61193ecb558d489e09de1762ba592437ac0b0
Hilal Ezgi Özlem <hilal_ezgi_2003@hotmail.com>
2026-04-22 09:03:25 +0300

    move derived state calculations fromHome to todoContext

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,13 @@
 import { Box } from "@/components/ui/box";
 import Header from "@/components/Header/Header";
 import CreateTodo from "../components/CreateTodo/CreateTodo";
-import { useEffect, useMemo, useReducer, useState } from "react";
 import TodoList from "@/components/TodoList/TodoList";
 import { TodoStatus } from "@/types/todo";
-import { todoReducer } from "../store/todoReducer";
 import { useTodo } from "../store/todoContext";
 
 export default function Home() {
-  const { todos, dispatch } = useTodo();
-
-  const todoCount = useMemo(() => {
-    return todos.filter((t) => t.status !== TodoStatus.DONE).length;
-  }, [todos]);
-
-  const todoTasks = useMemo(
-    () => todos.filter((t) => t.status === TodoStatus.TODO),
-    [todos],
-  );
-  const inProgressTasks = useMemo(
-    () => todos.filter((t) => t.status === TodoStatus.IN_PROGRESS),
-    [todos],
-  );
-  const doneTasks = useMemo(
-    () => todos.filter((t) => t.status === TodoStatus.DONE),
-    [todos],
-  );
+  const { todos, todoCount, todoTasks, inProgressTasks, doneTasks, dispatch } =
+    useTodo();
 
   const handleCreateTodo = (title: string) => {
     dispatch({
--- store/todoContext.tsx
+++ store/todoContext.tsx
@@ -4,12 +4,17 @@ import React, {
   ReactNode,
   useContext,
   Dispatch,
+  useMemo,
 } from "react";
 import { todoReducer, todoAction } from "./todoReducer";
 import { TodoStatus } from "@/types/todo";
 
 interface TodoContextType {
   todos: any[];
+  todoCount: number;
+  todoTasks: any[];
+  inProgressTasks: any[];
+  doneTasks: any[];
   dispatch: React.Dispatch<todoAction>;
 }
 export const TodoContext = createContext<TodoContextType | undefined>(
@@ -18,8 +23,34 @@ export const TodoContext = createContext<TodoContextType | undefined>(
 export const TodoProvider = ({ children }: { children: ReactNode }) => {
   const [todos, dispatch] = useReducer(todoReducer, []);
 
+  const todoCount = useMemo(() => {
+    return todos.filter((t) => t.status !== TodoStatus.DONE).length;
+  }, [todos]);
+
+  const todoTasks = useMemo(
+    () => todos.filter((t) => t.status === TodoStatus.TODO),
+    [todos],
+  );
+  const inProgressTasks = useMemo(
+    () => todos.filter((t) => t.status === TodoStatus.IN_PROGRESS),
+    [todos],
+  );
+  const doneTasks = useMemo(
+    () => todos.filter((t) => t.status === TodoStatus.DONE),
+    [todos],
+  );
+
   return (
-    <TodoContext.Provider value={{ todos, dispatch }}>
+    <TodoContext.Provider
+      value={{
+        todos,
+        dispatch,
+        todoCount,
+        todoTasks,
+        inProgressTasks,
+        doneTasks,
+      }}
+    >
       {children}
     </TodoContext.Provider>
   );
